bebf6bd83303201a5165893cdc053cf9d44c1296
AyeChanPyaeSone <ayechanpyaesone.ac@gmail.com>
2015-06-20 23:01:27 +0800

    Initial Commit

---
+++ testingusingioscharts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testingusingioscharts
+//
+//  Created by AyeChan PyaeSone on 20/6/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testingusingioscharts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testingusingioscharts
+//
+//  Created by AyeChan PyaeSone on 20/6/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
